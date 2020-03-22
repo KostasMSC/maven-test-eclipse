92510dcaf8cbcb3e4f7b541e3f0a242f6245645a
Kostas Argyris <konstantinos.argyris@intrasoft-intl.com>
2020-03-22 14:48:47 +0200

    first commit

---
+++ src/test/java/gr/athtech/maven_test_eclipse/AllTests.java
@@ -0,0 +1,16 @@
+package gr.athtech.maven_test_eclipse;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite(AllTests.class.getName());
+		//$JUnit-BEGIN$
+		suite.addTest(AppTest.suite());
+		//$JUnit-END$
+		return suite;
+	}
+
+}
